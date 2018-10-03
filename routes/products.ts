d6c783ca35b81d921c7c30a174fd213b6555aeee
hr5t5@windowslive.com <hr5t5@windowslive.com>
2018-10-03 13:28:17 +0200

    fix products post response

--- routes/products.ts
+++ routes/products.ts
@@ -8,54 +8,69 @@ import moment from 'moment';
 // probably need to move full array at once
 
 const router = express.Router();
+let updatingDb = false;
 
 router.get('/', (req: any, res: any, next: any) => {
     Product.getAll((err: any, items: any) => {
         if (err) console.log(err);
         else res.json(items);
     });
+
+
 });
 
 
-
 router.post('/', (req: any, res: any, next: any) => {
-    Time.get((err: any, response: any) => {
-        if (err) console.error(err);
-        else if (response.length) {
-            const duration = moment.duration(moment(new Date()).diff(moment(response[0].time)));
-            if (duration.asMinutes() > 5) {
-                Time.update(new Date(), (err: any) => {
-                    if (err) console.error(err);
-                    else {
-                        main.product.list = [];
-                        main.product.index = 1;
-                        main.scrapAll().then(() => {
-                            Product.addAll(main.product.list, (err: any) => {
-                                if (err) console.log(err);
-                                else res.json(main.product.list);
+    if (!updatingDb) {
+        updatingDb = true;
+        Time.get((err: any, response: any) => {
+            if (err) console.error(err);
+            else if (response.length) {
+                const duration = moment.duration(moment(new Date()).diff(moment(response[0].time)));
+                if (duration.asMinutes() > 5) {
+                    Time.update(new Date(), (err: any) => {
+                        if (err) console.error(err);
+                        else {
+                            main.product.list = [];
+                            main.product.index = 1;
+                            main.scrapAll().then(() => {
+                                Product.addAll(main.product.list, (err: any) => {
+                                    if (err) console.log(err);
+                                    else {
+                                        updatingDb = false;
+                                        res.json(0);
+                                    }
+                                });
                             });
-                        });
-                    }
-                });
-            } else {
-                res.json((5 - duration.asMinutes()).toFixed(2));
+                        }
+                    });
+                } else {
+                    updatingDb = false;
+                    res.json((5 - duration.asMinutes()).toFixed(2));
+                }
             }
-        }
-        else {
-            Time.add(new Date(), (err: any) => {
-                if (err) throw err;
-                main.product.list = [];
-                main.product.index = 1;
-                main.scrapAll().then(() => {
-                    Product.addAll(main.product.list, (err: any) => {
-                        if (err) console.log(err);
-                        else res.json(main.product.list);
+            else {
+                Time.add(new Date(), (err: any) => {
+                    if (err) throw err;
+                    main.product.list = [];
+                    main.product.index = 1;
+                    main.scrapAll().then(() => {
+                        Product.addAll(main.product.list, (err: any) => {
+                            if (err) console.log(err);
+                            else {
+                                updatingDb = false;
+                                res.json(0);
+                            }
+                        });
                     });
                 });
-            });
-        }
+            }
 
-    });
+        });
+    } else {
+        res.json(5);
+    }
+
 
 });
 
